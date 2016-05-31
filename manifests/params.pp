d6416901f7676ca53d0841c6967bc30f46160e85
Adam Breaux <breauxaj@gmail.com>
2016-05-31 08:32:06 -0400

    simplify

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,37 +8,10 @@
 class lsb::params {
   case $::osfamily {
     'Debian': {
-      case $::operatingsystemmajrelease {
-        '8': {
-          $lsb_package = 'lsb-release'
-        }
-        default: {
-          fail("The ${module_name} module is not supported on an ${::operatingsystem}${::operatingsystemmajrelease} distribution.")
-        }
-      }
+      $lsb_package = 'lsb-release'
     }
     'RedHat': {
-      case $::operatingsystemmajrelease {
-        '6': {
-          $lsb_package = 'redhat-lsb'
-        }
-        '7': {
-          $lsb_package = 'redhat-lsb'
-        }
-        default: {
-          fail("The ${module_name} module is not supported on an ${::operatingsystem}${::operatingsystemmajrelease} distribution.")
-        }
-      }
-    }
-    'Linux': {
-      case $::operatingsystem {
-        'Amazon': {
-          $lsb_package = 'redhat-lsb'
-        }
-        default: {
-          fail("The ${module_name} module is not supported on an ${::operatingsystem} distribution.")
-        }
-      }
+      $lsb_package = 'redhat-lsb'
     }
     default: {
       fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
